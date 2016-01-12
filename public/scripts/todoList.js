71745e7624c7a981ce3213412f5e5abfdf022e93
huangliejin <huangliejin@wps.cn>
2016-01-12 15:38:43 +0800

    Pre : second commit

--- public/scripts/todoList.js
+++ public/scripts/todoList.js
@@ -1,25 +1,63 @@
 var style = {
 	color:"#DBE01B",
 	border: "1px #000 solid",
+	height:"100%"
 };
 
-var todoThings = "firstThing at 1:00:00";
+var cStyle = {
+	// font-size:"200%",
+	border: "1px #000 solid",
+	height:"50%",
+	textAlign:"center"
+};
 
 var TodoList = React.createClass({
+    getDefaultProps: function () {
+	    console.log("getDefaultProps, 1");
+	    return 
+	    {
+		    name : "prop name"
+		};
+    },
+    getInitialState: function () {
+	    console.log("getInitialState, 22222222");
+	    return {
+			nameList:[
+    			"Tom",
+    			"Jony"
+    		],
+	    };
+	},
 	render: function() {
+		console.log('render, 33333333');
+		var tString = '';
+		for (var pStr of this.state.nameList){
+			tString = tString + pStr + "\n";
+		}
+		console.log(tString);
 		return (
-			<p>{todoThings}</p>
+			<div style={style}>
+				<div style={cStyle}>{tString}</div>
+				<InputTodoContent></InputTodoContent>
+			</div>
 		);
 	}
 });
 
 var InputTodoContent = React.createClass({
 	render: function() {
-		return (<p>xxx</p>);
+		return (
+			<div>
+		        <button id="add"></button>
+		        <input id="newTodoContent"></input>
+			</div>
+		);
 	}
 });
 
 ReactDOM.render(
-  <div style={style}><TodoList></TodoList></div>,
-  document.getElementById('midView')
+  <div style={style}>
+  	<TodoList></TodoList>
+  </div>,
+  document.getElementById('contentView')
 );
